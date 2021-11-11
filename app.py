f49a0b56b0bf526edf0e4f47f7c7d82ede5759a0
Alice870429 <91588867+Alice870429@users.noreply.github.com>
2021-11-11 13:54:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,54 +14,7 @@ app = Flask(__name__)
 # 這個服務對應的方法
 def basic_url():
     # return 來回傳使用者要收到的資料
-    return '<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"></meta>
-    <title>安安</title>
-    <style type="text/css">
-    .head{
-        background-color: #005AB5; color: white; font-weight: bold; font-size: 20px;
-        text-align: center; padding: 10px;
-    }
-
-    .content{
-        width:1000px; margin-left: auto; margin-right: auto;
-    }
-
-    .box{
-        width: 280px; padding:10px; margin: 10px; background-color: white;
-        display: inline-block; vertical-align: top;
-        text-align: center;  height: 150px; line-height: 30px;
-    }
-
-    .title{font-weight: bold;}
-    </style>
-</head>
-<body style="margin: 0px; background-color: #EEEEEE;">
-    <div class="head">陳欣婷 Laura Chen</div>
-    <div class="content">
-        <div class="box">
-            <div class="title">我的基本資料</div>
-            <div>安安你好</div>
-            <div>安安你好</div>
-            <div>安安你好</div>
-        </div>
-        <div class="box">
-            <div class="title">我的個人興趣</div>
-            <div>安安你好</div>
-            <div>安安你好</div>
-            <div>安安你好</div>
-        </div>
-        <div class="box">
-            <div class="title">觀迎與我聯絡</div>
-            <div>安安你好</div>
-            <div>安安你好</div>
-            <div>安安你好</div>
-        </div>
-    </div>
-</body>
-</html>'
+    return '<h1>Hello 我是Laura!</h1><p>是目前就讀陽明交通大學管理科學所的碩二生——陳欣婷，也可以叫我Laura。同時也是ASML的IT部門實習生，工作內容主要是在幫工程師進行軟硬體除錯，除此之外也正在進行資料庫及庫存系統的建立，方便部門進行查帳，而透過這個系統幫助部門成功節省約百分之五十的時間在進行查帳。</p>'
 
 
 @app.route("/map/w01-6", methods=['GET'])
